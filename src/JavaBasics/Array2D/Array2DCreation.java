8ef74c2e21b4796cc6111a97701bfd6bf4800bfd
Gagan41 <gagant.p100@gmail.com>
2025-07-31 12:04:42 +0530

    Updated with Transpose of a matrix

--- src/JavaBasics/Array2D/Array2DCreation.java
+++ src/JavaBasics/Array2D/Array2DCreation.java
@@ -20,5 +20,14 @@ public class Array2DCreation {
             }
             System.out.println();
         }
+
+        System.out.println("Transpose of a matrix:");
+        for(int i=0; i<2; i++){
+            for (int j=0; j<2; j++){
+                System.out.print(matrix[j][i]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
