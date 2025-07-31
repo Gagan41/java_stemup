5c6f2374ab687102ad24235215fd07c409d817f1
Gagan41 <gagant.p100@gmail.com>
2025-07-31 11:49:50 +0530

    2D Array sum of Elements

---
+++ src/JavaBasics/Array2D/SumOfElements.java
@@ -0,0 +1,27 @@
+package JavaBasics.Array2D;
+import java.util.Scanner;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int [][] matrix = new int [3][3];
+        int sum = 0;
+
+        System.out.println("Enter the elements of the matrix:");
+        for(int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                matrix [i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix is:");
+        for(int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                sum += matrix[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
