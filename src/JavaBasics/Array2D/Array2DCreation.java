ec2f34e8358e799bcbee145e946e2cfd3b80c16c
Gagan41 <gagant.p100@gmail.com>
2025-07-31 11:39:43 +0530

    2D Array creation

---
+++ src/JavaBasics/Array2D/Array2DCreation.java
@@ -0,0 +1,24 @@
+package JavaBasics.Array2D;
+import java.util.*;
+
+public class Array2DCreation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][]  matrix = new int [2][2];
+
+        System.out.println("Enter the elements of the matrix:");
+        for(int i=0; i<2; i++){
+            for (int j=0; j<2; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix is:");
+        for(int i=0; i<2; i++){
+            for (int j=0; j<2; j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
