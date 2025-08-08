5511b8d66ad70f1edd469e6802559015cafc3869
Gagan41 <gagant.p100@gmail.com>
2025-08-08 10:24:09 +0530

    Linear Search Algorithm

---
+++ src/Searching/LinearSearch.java
@@ -0,0 +1,19 @@
+package Searching;
+
+public class LinearSearch {
+    static int linear(int[] arr, int key){
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Checking index "+i+": "+arr[i]);
+            if(arr[i] == key) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+        int key = 30;
+        int res = linear(arr,key);
+        if(res!=-1) System.out.println("Element found at index: "+res);
+        else System.out.println("Element not found");
+    }
+}
