84dd74e67a524c00ea00f4f256dad2d647c03308
Gagan41 <gagant.p100@gmail.com>
2025-08-09 12:26:55 +0530

    reverese array

--- src/JavaBasics/array/ReverseString.java
+++ src/JavaBasics/array/ReverseString.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array elements");
+        System.out.println("Enter the array length");
         int n = sc.nextInt();
 
         String[] fruits = new String[n];
