9a1e8dd3b66fe75cebc0956d0ece3e3ebd80d6df
Gagan41 <gagant.p100@gmail.com>
2025-08-11 10:12:15 +0530

    Bubble Sort updated

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -2,15 +2,22 @@ package Sorting;
 public class BubbleSort {
     public static void bSort(int[] arr){
         int n = arr.length;
+        boolean swapped;
         for ( int i=0; i<n-1; i++){
-            for (int j=0; j<n-1-i; j++){
+            swapped = true;
+            for (int j=0; j<n-i-1; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
             }
             printArray(arr);
+            // If no two elements were
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
         }
     }
     public static void printArray(int [] arr){
